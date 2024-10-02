213a6a07c80a9c1417a840eb0614d7c2a6ed50f5
tahminaisdavletova <tahminaisdavletova@gmail.com>
2024-10-02 12:56:21 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscrall = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+let currentSlide = 0;
+
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function changeSlide(direction) {
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    } else if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+setInterval(() => {
+    changeSlide(1);
+}, 3000);
+
+showSlide(currentSlide);
